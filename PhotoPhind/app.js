0b5de106d7fce927aa62e5f88c298e1d8e2f302f
David Ankney III <61752976+AnkneyDavid83@users.noreply.github.com>
2020-06-13 02:39:47 -0400

    Add files via upload
    HTML and CSS files. Initial data fetch from Pexels API

---
+++ PhotoPhind/app.js
@@ -0,0 +1,21 @@
+const auth = "563492ad6f91700001000001a3985d11c840412384beb6b9941adda9";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+}
+
+curatedPhotos();
